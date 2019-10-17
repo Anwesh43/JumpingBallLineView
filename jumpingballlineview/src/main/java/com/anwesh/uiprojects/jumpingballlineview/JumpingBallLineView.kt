4bef616e61940351cb808814792b664dfb1905b1
Anwesh43 <anweshthecool0@gmail.com>
2019-10-17 09:25:28 +0530

    adding JBLNode class

--- jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
+++ jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
@@ -120,4 +120,46 @@ class JumpingBallLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JBLNode(var i : Int, val state : State = State()) {
+
+        private var next : JBLNode? = null
+        private var prev : JBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = JBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJBLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JBLNode {
+            var curr : JBLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
