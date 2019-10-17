83b2902c9bce362fffc1177d7e060e8b57bffdf0
Anwesh43 <anweshthecool0@gmail.com>
2019-10-17 09:03:06 +0530

    adding constants and libraries

--- jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
+++ jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.jumpingballlineview
 /**
  * Created by anweshmishra on 17/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val delay : Long = 30
+val scGap : Float = 0.01f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
