3ce31c8c6b24ede33f75d6ae0f93e8d004a54f3e
Anwesh43 <anweshthecool0@gmail.com>
2019-10-17 09:16:03 +0530

    adding JumpingBallLineView class

--- jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
+++ jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawJBLNode(i : Int, scale : Float, paint : Paint) {
     drawJumpingBallLine(scale, h * yOffsetFactor, size, paint)
     restore()
 }
+
+class JumpingBallLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
