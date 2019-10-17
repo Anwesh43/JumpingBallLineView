2c3cc82ad9bae252b649f38fc3e8aa6e5be9d827
Anwesh43 <anweshthecool0@gmail.com>
2019-10-17 09:13:31 +0530

    adding Canvas extension method

--- jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
+++ jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
@@ -19,7 +19,39 @@ val delay : Long = 30
 val scGap : Float = 0.01f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val maxDeg : Float = 180f
+val rFactor : Float = 4f
+val yOffsetFactor : Float = 0.4f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawJumpingBallLine(scale : Float, h : Float, size : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    val deg : Float = 180f * scale.divideScale(0, 2)
+    val y : Float = -(h - size) * Math.sin(deg * Math.PI / 180).toFloat()
+    val y1 : Float = h * scale.divideScale(1, 2)
+    save()
+    translate(0f, y)
+    drawCircle(0f, 0f, r, paint)
+    restore()
+    save()
+    translate(0f, y1)
+    drawLine(0f, -size, 0f, size, paint)
+    restore()
+}
+
+fun Canvas.drawJBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawJumpingBallLine(scale, h * yOffsetFactor, size, paint)
+    restore()
+}
