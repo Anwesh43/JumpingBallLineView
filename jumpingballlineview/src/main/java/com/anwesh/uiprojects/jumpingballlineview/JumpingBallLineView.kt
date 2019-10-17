c14aa5f16886049fe5877d4be6afe602681e3f44
Anwesh43 <anweshthecool0@gmail.com>
2019-10-17 09:17:37 +0530

    adding State class

--- jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
+++ jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
@@ -72,4 +72,24 @@ class JumpingBallLineView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
