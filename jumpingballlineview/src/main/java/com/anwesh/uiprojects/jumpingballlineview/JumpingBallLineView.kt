ba0c59fa62f747eb1e355825edcbd052415e3360
Anwesh43 <anweshthecool0@gmail.com>
2019-10-17 09:21:38 +0530

    adding Animator class

--- jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
+++ jumpingballlineview/src/main/java/com/anwesh/uiprojects/jumpingballlineview/JumpingBallLineView.kt
@@ -92,4 +92,32 @@ class JumpingBallLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
